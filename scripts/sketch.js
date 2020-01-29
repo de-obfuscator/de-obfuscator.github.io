d114ee48ffed73c9adc4f1df7a7bce55737f4878
Lance <ltrudel@monusb.ca>
2020-01-29 17:37:16 -0600

    initial

---
+++ scripts/sketch.js
@@ -0,0 +1,15 @@
+/// <reference path="../p5.global-mode.d.ts" />
+
+function setup(){
+    document.head.querySelector("[property~=video][content]").content;
+}
+
+function draw(){
+
+}
+function getMainArticle(){
+    
+}
+function getNewestArticle(){
+
+}
